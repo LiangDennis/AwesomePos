ee78bf5c881c2a53b761a49d80cc26bc68c87098
LiangDennis <18898513421@163.com>
2018-09-03 21:53:06 +0800

    pos.vue,icon,the template of leftNav,and the element.

---
+++ src/components/page/Pos.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="pos">
+        <el-row>
+            <el-col :span='7' class="pos-order" id="order-list">
+                我是订单栏
+            </el-col>
+            <el-col>
+                我是产品栏
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'pos',
+    mounted:function() {
+        var orderHeight =document.body.clientHeight;
+        console.log(orderHeight);
+        document.getElementById("order-list").style.height=orderHeight +"px";
+    }
+}
+</script>
+<style scoped>
+.pos-order {
+    background-color: #f9fafc;
+    border-right: 1px solid #c0ccda;
+}
+
+</style>
